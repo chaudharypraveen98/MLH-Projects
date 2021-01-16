b229802a4cc44274a9a405c59688e6a2b56d194a
praveen <chaudharypraveen98@gmail.com>
2021-01-16 14:02:52 +0530

    commit starva

---
+++ Starva/starvi_api.py
@@ -0,0 +1,25 @@
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "60029",
+    'client_secret': '63b3167bfe443f8fe02e06cb3d118a27be05ac2a',
+    'refresh_token': 'b2e7800734e17b2961d5be520a34e3313e020344',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
+param = {'per_page': 200, 'page': 1}
+my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+print(my_dataset)
